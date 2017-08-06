f5647a001c3c32c3526647e746343ea1e3d3246a
DangDung <dangdungcntt@gmail.com>
2017-08-06 18:35:55 +0700

    fix png, jpg imgUrl

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ const getImgUrl = (content) => {
   // console.log(content);
 
   const start = content.indexOf('src="') + 5;
-  const end = content.indexOf('.jpg') + 4;
+  const end = content.indexOf('g"', start) + 1;
 
   return content.substring(start, end);
 };
