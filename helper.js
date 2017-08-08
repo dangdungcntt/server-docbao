c2d416d5a65217753466ed55d646cc82b3ebf739
DangDung <dangdungcntt@gmail.com>
2017-08-08 14:26:33 +0700

    add default images if does not have images

--- helper.js
+++ helper.js
@@ -4,6 +4,9 @@ const getImgUrl = (content) => {
   const start = content.indexOf('src="') + 5;
   const end = content.indexOf('"', start + 10);
 
+  if (end < start) {
+    return 'https://docbao.tentstudy.xyz/images/default_news.jpg';
+  }
   return content.substring(start, end);
 };
 
