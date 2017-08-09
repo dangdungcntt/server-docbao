589b831f5dee60beda2613ae3c08405d0796c4ac
DangDung <dangdungcntt@gmail.com>
2017-08-09 09:44:44 +0700

    cached data in 50 seconds

--- helper.js
+++ helper.js
@@ -1,9 +1,35 @@
-const getImgUrl = (content) => {
-  // console.log(content);
+let cacheData = [];
 
+const getCacheData = (key) => {
+  const curTime = new Date().getTime();
+  const item = cacheData[key];
+  if (item && item.expire >= curTime) {
+    return {
+      success: true,
+      data: item.value
+    }
+  }
+  return {
+    success: false
+  }
+};
+
+const setCacheData = (key, value) => {
+  cacheData[key] = {
+    value,
+    expire: new Date().getTime() + 50 * 1000
+  };
+};
+
+const getNewData = (data, lastTime) => {
+  return data.filter((item) => {
+    return new Date(item.time).getTime() > lastTime;
+  });
+};
+
+const getImgUrl = (content) => {
   const start = content.indexOf('src="') + 5;
   const end = content.indexOf('"', start + 10);
-
   if (end < start) {
     return 'https://docbao.tentstudy.xyz/images/default_news.jpg';
   }
@@ -11,5 +37,8 @@ const getImgUrl = (content) => {
 };
 
 module.exports = {
-  getImgUrl
+  getImgUrl,
+  getCacheData,
+  setCacheData,
+  getNewData
 }
--- index.js
+++ index.js
@@ -21,42 +21,40 @@ app.post('/api/rss', (req, res) => {
       err: 'Please send with "fetchLink" "and lastTime"' 
     });
   }
+  const checkData = helper.getCacheData(fetchLink);
+  if (checkData.success) {
+    return res.json({
+      success: true,
+      data: helper.getNewData(checkData.data, lastTime),
+      lastTime: new Date(checkData.data[0].time).getTime(),
+      cached: true
+    });
+  }
   rssParser.parseURL(fetchLink, (err, parsed) => {
     if (err) return res.json({ success: false, err });
     const listNews = parsed.feed.entries;
-    if (new Date(listNews[0].pubDate).getTime() == lastTime) {
-      return res.json({
-        success: true,
-        data: [],
-        lastTime,
-        msg: 'up-to-date'
-      });
-    }
     let data = [];
     for (let i = 0; i < 3; i++) {
       const { title, link, contentSnippet, pubDate, content } = listNews[i];
       const imgUrl = helper.getImgUrl(content);
-      if (new Date(pubDate).getTime() > lastTime) {
-        data.push({
-          imgUrl,
-          title,
-          link,
-          des: contentSnippet,
-          time: pubDate
-        });
-      }
+      data.push({
+        imgUrl,
+        title,
+        link,
+        des: contentSnippet,
+        time: pubDate
+      });
     }
-    // console.log(new Date(listNews[0].pubDate).getTime());
+    helper.setCacheData(fetchLink, data);
+    const topTime = new Date(data[0].time).getTime();
     res.json({
       success: true,
-      data,
-      lastTime: new Date(listNews[0].pubDate).getTime()
+      data: helper.getNewData(data, lastTime),
+      lastTime: topTime,
+      cached: false
     });
-    // console.log(JSON.stringify(parsed));
-    
   })
 });
-
 
 const port = process.env.PORT || 3001;
 app.listen(port, (err) => {
